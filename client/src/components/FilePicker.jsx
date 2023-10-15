b917f7d88c18618a5fe4019ea1a3d56ee790124c
Mohamed Fawzi Abdellaoui <mohamedfawzi.abdellaoui@gmail.com>
2023-10-15 17:41:54 +0100

    [ADD] add client part

---
+++ client/src/components/FilePicker.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { reader } from '../config/helpers'
+
+const FilePicker = () => {
+  return (
+    <div>FilePicker</div>
+  )
+}
+
+
+
+
+export default FilePicker
